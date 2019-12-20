bee6e6ba7aaaa047cb9adcbc42553c834abea2ea
Carlos Nieto <cnieto@apple.com>
2019-12-20 13:58:12 -0600

    Micro setup

---
+++ facade/src/main/java/com/dm/facade/FacadeApp.java
@@ -0,0 +1,13 @@
+package com.dm.facade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacadeApp.class, args);
+	}
+
+}
--- service/src/main/java/com/dm/service/DemoApplication.java
+++ service/src/main/java/com/dm/service/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.dm.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
